fd012ab7eca9537fbb45e6f7bc7e2051296ab3c0
Md. Shofiqul Islam <shofiqebr@gmail.com>
2024-01-27 09:47:45 +0600

    home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,32 @@
+import { Box } from "@mui/material";
+
+
+const Home = () => {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        
+        }}>
+            <Box >
+
+            <h1>Image to PDF converter</h1>
+            <Box>
+
+            <input type="file" name="" accept=".png, .jpg, .jpeg"/>
+            Upload image
+            </Box>
+            </Box>
+            
+            <img style={{width:'300px', height: '300px'}} src="./no-photo-or-blank-image-icon-loading-images-vector-37375020.jpg " alt="" />
+           <button style={{margin: '10px'}}>
+
+       image to pdf
+           </button>
+        </Box>
+    );
+};
+
+export default Home;
